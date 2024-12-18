ba5ff5898e030c80d2cc410770e5a31ee48b972a
Rafik Khodr <158206235+rkhodr@users.noreply.github.com>
2024-12-18 15:08:27 -0500

    Update DiscreteMath,.java

--- DiscreteMath,.java
+++ DiscreteMath,.java
@@ -1,94 +1,7 @@
 import java.util.Scanner;
 
 public class DiscreteMath {
-     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter number of 'balls': ");
-        int balls = scanner.nextInt();
-
-        System.out.print("Enter number of 'boxes': ");
-        int boxes = scanner.nextInt();
-
-        System.out.print("Are balls distinct? (true/false): ");
-        boolean ballsDistinct = scanner.nextBoolean();
-
-        System.out.print("Are boxes distinct? (true/false): ");
-        boolean boxesDistinct = scanner.nextBoolean();
-
-        System.out.print("Enter restrictions: n- for none, i- (injective) for at Most One, s- (surjective) for at Least One): ");
-        String restriction = scanner.next();
-
-        String result = calculate(balls, boxes, ballsDistinct, boxesDistinct, restriction);
-        System.out.println("Number of variations: " + result);
-    }
-
-
-
-    public static String calculate(int n, int x, boolean ball, boolean box, String res){
-
-        //SECTION 1- NO LIMITATIONS
-        if((res.equals("n"))){
-            if(ball && box){
-                return "This is a multiplication principle problem. The total variations would be " + (int)Math.pow(x,n);
-            }
-            else if(!ball && box){
-                return "This is a combinations with repetition problem. The total variations would be " + (int)combinations(n+x-1,n);
-            }
-            else if(ball && !box){
-                return "This is a sum of set partition problem. The total variations would be " + (int)partitionsIntoAtMostXBoxes(n,x);
-            }
-            else if(!ball && !box){
-                return "This is an integer partition problem. The total variations would be " + countPartitions(n+x);
-            }
-        }
-
-
-
-        //SECTION 2- INJECTIVE
-        if((res.equals("i"))){
-            if(ball && box){
-                return "This is a permutations problem. The total variations would be " + (int)permutations(n,x);
-            }
-            else if(!ball && box){
-                return "This is a regular combinations. The total variations would be " + (int)combinations(n,x);
-            }
-            else if(ball && !box){
-                if(n>x) return "With if there are more balls than boxes, but you can have at most one ball in each box, this is impossible (using every ball), so answer is 0";
-                else return  "With there being an equal or lesser number of balls compared to indentical box, theres only one way to put them since the boxes are the same, so answer is 1";
-            }
-            else if(!ball && !box){
-                if(n>x) return "With if there are more balls than boxes, but you can have at most one ball in each box, this is impossible (using every ball), so answer is 0";
-                else return "With there being an equal or lesser number of balls compared to indentical box, theres only one way to put them since the boxes are the same, so answer is 1";
-            }
-        }
-
-
-
-        //SECTION 3- SURJECTIVE
-        if((res.equals("s"))){
-            if(ball && box){
-                return "This is a labeled set partition problem. The total variations would be " + (int)factorial(x) * (int)stirlingNumber(n,x);
-            }
-            else if(!ball && box){
-                return "This is a combinations without repetition problem. The total variations would be " + (int)combinations(n-1,x-1);
-            }
-            else if(ball && !box){
-                return "This is a set partition problem. The total variations would be " + stirlingNumber(n,x);
-            }
-            else if(!ball && !box){
-                return "This is a integer partition problem. The total variations would be " + countPartitions(n);
-            }
-        }
-        return "Invalid Input somewhere. Try again with valid inputs";
-    }
-
-
-
-
-
-
-
+     
     public static long factorial(int n) {
         long result = 1;
         for (int i = 1; i <= n; i++) {
@@ -155,5 +68,89 @@ public class DiscreteMath {
         }
 
         return result;
+    }
+
+
+
+       public static String calculate(int n, int x, boolean ball, boolean box, String res){
+
+        //SECTION 1- NO LIMITATIONS
+        if((res.equals("n"))){
+            if(ball && box){
+                return "This is a multiplication principle problem. The total variations would be " + (int)Math.pow(x,n);
+            }
+            else if(!ball && box){
+                return "This is a combinations with repetition problem. The total variations would be " + (int)combinations(n+x-1,n);
+            }
+            else if(ball && !box){
+                return "This is a sum of set partition problem. The total variations would be " + (int)partitionsIntoAtMostXBoxes(n,x);
+            }
+            else if(!ball && !box){
+                return "This is an integer partition problem. The total variations would be " + countPartitions(n+x);
+            }
+        }
+
+
+
+        //SECTION 2- INJECTIVE
+        if((res.equals("i"))){
+            if(ball && box){
+                return "This is a permutations problem. The total variations would be " + (int)permutations(n,x);
+            }
+            else if(!ball && box){
+                return "This is a regular combinations. The total variations would be " + (int)combinations(n,x);
+            }
+            else if(ball && !box){
+                if(n>x) return "With if there are more balls than boxes, but you can have at most one ball in each box, this is impossible (using every ball), so answer is 0";
+                else return  "With there being an equal or lesser number of balls compared to indentical box, theres only one way to put them since the boxes are the same, so answer is 1";
+            }
+            else if(!ball && !box){
+                if(n>x) return "With if there are more balls than boxes, but you can have at most one ball in each box, this is impossible (using every ball), so answer is 0";
+                else return "With there being an equal or lesser number of balls compared to indentical box, theres only one way to put them since the boxes are the same, so answer is 1";
+            }
+        }
+
+
+
+        //SECTION 3- SURJECTIVE
+        if((res.equals("s"))){
+            if(ball && box){
+                return "This is a labeled set partition problem. The total variations would be " + (int)factorial(x) * (int)stirlingNumber(n,x);
+            }
+            else if(!ball && box){
+                return "This is a combinations without repetition problem. The total variations would be " + (int)combinations(n-1,x-1);
+            }
+            else if(ball && !box){
+                return "This is a set partition problem. The total variations would be " + stirlingNumber(n,x);
+            }
+            else if(!ball && !box){
+                return "This is a integer partition problem. The total variations would be " + countPartitions(n);
+            }
+        }
+        return "Invalid Input somewhere. Try again with valid inputs";
+    }
+
+
+
+        public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter number of 'balls': ");
+        int balls = scanner.nextInt();
+
+        System.out.print("Enter number of 'boxes': ");
+        int boxes = scanner.nextInt();
+
+        System.out.print("Are balls distinct? (true/false): ");
+        boolean ballsDistinct = scanner.nextBoolean();
+
+        System.out.print("Are boxes distinct? (true/false): ");
+        boolean boxesDistinct = scanner.nextBoolean();
+
+        System.out.print("Enter restrictions: n- for none, i- (injective) for at Most One, s- (surjective) for at Least One): ");
+        String restriction = scanner.next();
+
+        String result = calculate(balls, boxes, ballsDistinct, boxesDistinct, restriction);
+        System.out.println("Number of variations: " + result);
     }
 }
